1052a66cd882b5977766b0a58c0b72172e7e41aa
George Ciobanu <geo.ciobanu@gmail.com>
2012-05-01 03:20:23 -0700

    : does not denote a var

--- backend/app/jobs/mail_job.rb
+++ backend/app/jobs/mail_job.rb
@@ -2,7 +2,7 @@ class MailJob < Struct.new(:contact_id)
   def perform
     Rails.logger.info("Performing AttackJob")
     # get the info for that contact
-    c = Contact.find_by_id :contact_id
+    c = Contact.find contact_id
     Rails.logger.info  c
     c.remind
   end
