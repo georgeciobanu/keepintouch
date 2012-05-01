f9f3e5b9bac42d7eb577c0f0a7a5d532d9795121
George Ciobanu <geo.ciobanu@gmail.com>
2012-04-30 23:28:07 -0700

    Clean up

--- backend/app/jobs/MailJob.rb
+++ backend/app/jobs/MailJob.rb
@@ -4,6 +4,5 @@ class MailJob < Struct.new(:contact_id)
     # get the info for that contact
     Contact.find_by_id :contact_id
     Contact.remind 
-    # make a POST request to MailGun to send the email
   end
 end
