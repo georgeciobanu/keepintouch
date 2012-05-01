332d0e8422f6296e26d85678f4aabe8393efb299
George Ciobanu <geo.ciobanu@gmail.com>
2012-05-01 03:11:38 -0700

    Instance not Class method

--- backend/app/jobs/mail_job.rb
+++ backend/app/jobs/mail_job.rb
@@ -2,7 +2,8 @@ class MailJob < Struct.new(:contact_id)
   def perform
     Rails.logger.info("Performing AttackJob")
     # get the info for that contact
-    Contact.find_by_id :contact_id
-    Contact.remind 
+    c = Contact.find_by_id :contact_id
+    Rails.logger.info  c
+    c.remind
   end
 end
